b5dd6c52b31f9d1d4ff4ca4f6037a8b34a5b261b
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-07-02 20:17:37 +0200

    Got the slider to work
    It was the bundler it does not accept min js formats.
    
    Added fake non min js files
    
    The bundler do not by default care about min files when in debug.

--- src/app/TrainingCamp.Web/App_Start/BundleConfig.cs
+++ src/app/TrainingCamp.Web/App_Start/BundleConfig.cs
@@ -8,61 +8,45 @@ namespace TrainingCamp.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //Google Api map first
+
+            // Css for customization
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/layout.css"));
+            //Bundles for my superweb toolkit 
+            bundles.Add(new StyleBundle("~/Extlibs/superweb/css").Include(
+                "~/Extlibs/bootstrap/css/bootstrap.css",
+                // plugin css
+                "~/Extlibs/js-plugin/pretty-photo/css/prettyPhoto.css",
+                "~/Extlibs/js-plugin/flexslider/flexslider.css",              
+                "~/Extlibs/js-plugin/one_by_one/css/jquery.onebyone.css",
+                "~/Extlibs/js-plugin/one_by_one/css/animate.css",
+                "~/Extlibs/js-plugin/neko-contact-ajax-plugin/css/cmxformTemplate.css",
+                // icon fonts
+                "~/Extlibs/font-icons/custom-icons/css/custom-icons.css"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/layout.css",
-                "~/Content/color.css"
-                            ));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                "~/Content/themes/base/jquery.ui.core.css",
-                "~/Content/themes/base/jquery.ui.resizable.css",
-                "~/Content/themes/base/jquery.ui.selectable.css",
-                "~/Content/themes/base/jquery.ui.accordion.css",
-                "~/Content/themes/base/jquery.ui.autocomplete.css",
-                "~/Content/themes/base/jquery.ui.button.css",
-                "~/Content/themes/base/jquery.ui.dialog.css",
-                "~/Content/themes/base/jquery.ui.slider.css",
-                "~/Content/themes/base/jquery.ui.tabs.css",
-                "~/Content/themes/base/jquery.ui.datepicker.css",
-                "~/Content/themes/base/jquery.ui.progressbar.css",
-                "~/Content/themes/base/jquery.ui.theme.css"));
-            //Bundles for my superweb toolkit 
-            bundles.Add(new StyleBundle("~/extlibs/superweb/css").Include(
-                "~/extlibs/bootstrap/css/bootstrap.css",
-// plugin css
-                "~/extlibs/js-plugin/pretty-photo/css/prettyPhoto.css",
-                "~/extlibs/js-plugin/one_by_one/css/jquery.onebyone.css",
-                "~/extlibs/js-plugin/one_by_one/css/animate.css",
-// icon fonts
-                "~/extlibs/font-icons/custom-icons/css/custom-icons.css"
-            ))
-            ;
-            bundles.Add(new ScriptBundle("~/extlibs/superweb/js").Include(
-            "~/extlibs/js-plugin/respond/respond.min.js",
-            "~/extlibs/js-plugin/jquery-ui/jquery-ui-1.8.23.custom.min.js",
-                "~/extlibs/bootstrap/js/bootstrap.js",
-                "~/extlibs/js-plugin/waypoint/waypoints.min.js",
-                "~/extlibs/js-plugin/waypoint/sticky/waypoints-sticky.min.js",
+ 
+            bundles.Add(new ScriptBundle("~/Extlibs/superweb/js").Include(
+                "~/Extlibs/js-plugin/respond/respond.src.js",
+                "~/Extlibs/js-plugin/jquery-ui/jquery-ui-1.8.23.custom.js",
+                //Third party plubins //Third party plubins 
+                "~/Extlibs/bootstrap/js/bootstrap.js",
+                "~/Extlibs/js-plugin/easing/jquery.easing.1.3.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Extlibs/js-plugin/waypoint/waypoints.js",
+                "~/Extlibs/js-plugin/waypoint/sticky/waypoints-sticky.js",
                 // One by one Slider  
-                "~/extlibs/js-plugin/one_by_one/jquery.onebyone.min.js",
-                "~/extlibs/js-plugin/one_by_one/jquery.touchwipe.min.js",
+                "~/Extlibs/js-plugin/one_by_one/jquery.onebyone.js",
+                "~/Extlibs/js-plugin/one_by_one/jquery.touchwipe.js",
                 //Custom
-               "~/Scripts/custom.js"
+                "~/Scripts/custom.js"
                             )
                 );
         }
