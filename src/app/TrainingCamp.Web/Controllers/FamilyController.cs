1a667630f50dc69f45875fcd9ba0c4bae0266820
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-07-04 17:11:04 +0200

    Fixing basic views
    For Camp, Travel ,Family and language menu

---
+++ src/app/TrainingCamp.Web/Controllers/FamilyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingCamp.Web.Controllers
+{
+    public class FamilyController : Controller
+    {
+        //
+        // GET: /Family/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
