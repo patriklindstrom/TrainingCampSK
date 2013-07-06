af4955d64ad0f190ccdcc9ed30bb2c25dc027aaa
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-07-06 22:55:27 +0200

    fixing home page
    and fake blog page

--- src/app/TrainingCamp.Web/Controllers/BlogController.cs
+++ src/app/TrainingCamp.Web/Controllers/BlogController.cs
@@ -16,5 +16,9 @@ namespace TrainingCamp.Web.Controllers
             return View();
         }
 
+        public ActionResult post(string blogkey)
+        {
+            return View();
+        }
     }
 }
