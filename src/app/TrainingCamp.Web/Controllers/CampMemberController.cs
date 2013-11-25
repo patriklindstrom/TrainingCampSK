a3b4d5eefe5ec833f96cb2efcee6fac011d1c8fe
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-11-25 22:54:51 +0100

    Add CampMember
    Admin pages for CRUD of Campmembers

---
+++ src/app/TrainingCamp.Web/Controllers/CampMemberController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingCamp.Web.Controllers
+{
+    public class CampMemberController : Controller
+    {
+        //
+        // GET: /CampMember/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /CampMember/Details/5
+
+        public ActionResult Details(string cid)
+        {
+            return View();
+        }
+
+        //
+        // GET: /CampMember/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /CampMember/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CampMember/Edit/5
+
+        public ActionResult Edit(string cid)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CampMember/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(string cid, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CampMember/Delete/5
+
+        public ActionResult Delete(string cid)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CampMember/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(string cid, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/app/TrainingCamp.Web/Models/CampMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainingCamp.Web.Models
+{
+    public class CampMember
+    {
+        [DisplayName("KenshiNr")]
+        [Key]
+        [Required]
+        public string CampMemberId { get; set; }
+        [DisplayName("Club Nr")]
+        public string ClubNr { get; set; }
+        [DisplayName("Club Name")]
+        public string ClubName { get; set; }
+        public string Name { get; set; }
+        [RegularExpression(@"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}\b")]
+        public string Email { get; set; }
+        public bool Paid { get; set; }
+        public float PaidAmount { get; set; }
+        public string PaidCurrency { get; set; }
+        public DateTime PaidDateTime { get; set; }
+        public string Comment { get; set; }
+    }
+}
